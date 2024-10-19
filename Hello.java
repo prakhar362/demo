6bc54af33a9536a3c12da31e5923fc0d3a7adb43
prakhar362 <145253827+prakhar362@users.noreply.github.com>
2024-10-19 13:44:00 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
     public static void main(String[] args)
     {
-        System.out.println(Prakhar S. testing java prac7);
+        System.out.println("Prakhar S. testing java prac7");
     }
 }
